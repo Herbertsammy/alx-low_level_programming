64f2a5b7cf9a8eed615906b45544ea1005676fd8
herbert samuel <herbertsammy360@gmail.com>
2022-09-09 21:37:44 -0700

    3 print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 succesful
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	putchar(tolower(x));
+	}
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	putchar(x);
+	}
+
+	putchar('\n');
+	return (0);
+}
