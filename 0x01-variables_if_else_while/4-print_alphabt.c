32b4ad75611ead645618c0658930c5f5c29bd32a
herbert samuel <herbertsammy360@gmail.com>
2022-09-09 21:42:46 -0700

    The 4th print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; ++x)
+	{
+	if (x == 'e' || x == 'q')
+	{
+	x = x;
+	} else
+	{
+	putchar(x);
+	}
+	}
+
+	putchar('\n');
+	return (0);
+}
