a21cac3434761b8b01ccfc90b794fff129a1dde2
herbert samuel <herbertsammy360@gmail.com>
2022-09-09 21:31:38 -0700

    The 2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+	putchar(tolower(c));
+	}
+
+	putchar('\n');
+	return (0);
+}
