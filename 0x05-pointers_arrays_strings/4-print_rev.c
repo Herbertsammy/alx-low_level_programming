2d1cbb52d5fb5fda5860341815e5a6ce27c169ca
herbert samuel <herbertsammy360@gmail.com>
2022-09-20 22:00:01 -0700

    The 4 print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - function that prints string in reverse direction
+ * @s: pointer to the string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	char temp[10000];
+	int i, j, count;
+
+	while (s[count] != '\0')
+	{
+	count++;
+	}
+
+	j = count - 1;
+	for (i = 0; i < count; i++)
+	{
+	temp[i] =  s[j];
+	j--;
+	}
+
+	puts(temp);
+}
