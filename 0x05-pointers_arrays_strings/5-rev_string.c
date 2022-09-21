00ce62951e61b08256fd404e6cef2f560efc189e
herbert samuel <herbertsammy360@gmail.com>
2022-09-20 22:05:12 -0700

    The 5th string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function that reverses string
+ * @s: pointer to input string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	char x;
+	int i;
+
+	int n = strlen(s);
+
+	for (i = 0; i < n / 2; i++)
+	{
+	x = s[i];
+	s[i] = s[n - i - 1];
+	s[n - i - 1] = x;
+	}
+}
