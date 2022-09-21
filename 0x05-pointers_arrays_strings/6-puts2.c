dcb2d6b477e1fb217a384e1f02188f3380c78c0b
herbert samuel <herbertsammy360@gmail.com>
2022-09-20 22:10:11 -0700

    The 6th string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - prints every other character starting from the first
+ * @str: pointer to input string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	int len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
+	{
+	putchar(str[i]);
+	}
+
+	putchar('\n');
+}
